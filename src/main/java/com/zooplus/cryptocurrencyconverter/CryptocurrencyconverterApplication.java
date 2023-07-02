60571c4b98ccd7a1dd7bdefdd69bc9ecbd8f021e
Praveen <prav_gali@yahoo.com>
2023-07-02 17:15:00 +0530

    init

---
+++ src/main/java/com/zooplus/cryptocurrencyconverter/CryptocurrencyconverterApplication.java
@@ -0,0 +1,13 @@
+package com.zooplus.cryptocurrencyconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocurrencyconverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zooplus/cryptocurrencyconverter/CryptocurrencyconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zooplus.cryptocurrencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
